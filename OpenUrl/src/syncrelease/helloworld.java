0c6e56d5574a876e674858876b75f9f8948f27bd
gupteswar <rkblgupteswaro@gmail.com>
2017-09-10 14:52:56 +0530

    Commited modification

---
+++ OpenUrl/src/syncrelease/helloworld.java
@@ -0,0 +1,12 @@
+package syncrelease;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+		System.out.println("Commited");
+       
+	}
+
+}
